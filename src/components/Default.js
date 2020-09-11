6a80b9f27538c9218c8cdd97561101f9696bea83
JoinGithub500 <66752532+JoinGithub500@users.noreply.github.com>
2020-09-11 22:00:39 +0300

    Finished Working to the product(card) liste

---
+++ src/components/Default.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Default extends Component {
+    render() {
+        return (
+            <div>
+                 <h2>page Not Found</h2>
+            </div>
+        )
+    }
+}
---
+++ src/components/Detalis.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Detalis extends Component {
+    render() {
+        return (
+            <div>
+                 <h2>Hello from Detalis</h2>
+            </div>
+        )
+    }
+}
