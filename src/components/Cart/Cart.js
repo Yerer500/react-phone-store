bbcf7d5f66c59cb7d8de3ceabe183becda0effbc
JoinGithub500 <66752532+JoinGithub500@users.noreply.github.com>
2020-09-19 00:00:02 +0300

    paypalbutton adding

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -19,7 +19,7 @@ export default class Cart extends Component {
                   <Title name="your" title="cart" />
                   <CartColumns />
                   <CartList value={value} />
-                  <CartTotlas value={value} />
+                  <CartTotlas value={value} history={this.props.history} />
                 </React.Fragment>
               );
             } else {
